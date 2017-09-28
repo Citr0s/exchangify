40f1c3caad5076654cc08c7b3464800f203c230c
Milosz Dura <milosz.md@wp.pl>
2017-09-28 11:37:59 +0100

    moving users resource behing an api layer

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 $router = new RouterService(array_values(array_filter(explode('/', $_SERVER['REQUEST_URI']))), $_SERVER['REQUEST_METHOD']);
 
-$router->subscribe('users', 'GET', UsersController::class);
+$router->subscribe('api/users', 'GET', UsersController::class);
 
 $router->notify();
